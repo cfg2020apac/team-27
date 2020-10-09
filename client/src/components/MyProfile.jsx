4c7117ea421a1bec61beb39d25e0edc00273d6db
Jeff <jeffli351999@gmail.com>
2020-10-10 00:43:25 +0800

    Update MyProfile

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -18,11 +18,28 @@ export default function MyProfile(){
                     <Card>
                         <Card.Header>
                             <Accordion.Toggle as={Button} variant="light" eventKey="0">
-                                About you
+                                About Kevin
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
-                            <Card.Body>Need to connect to database</Card.Body>
+
+                     
+                            <Card fluid>
+                                <Card.Body>
+                                    <Card.Title>Full Name : Kevin Kim</Card.Title>
+                                    <Card.Title>School : Mr Handsome College </Card.Title>
+                                    <Card.Title>Year of joining : 2020</Card.Title>
+                                    <Card.Title>Year of study : 10 </Card.Title>
+                                </Card.Body>
+                                <Card.Body>
+                      
+                 
+                                    <Card.Link href="#">Edit Your Profile</Card.Link>
+                                </Card.Body>
+                                
+                            </Card>
+                            
+
                         </Accordion.Collapse>
                     </Card>
                     <Card>
@@ -32,13 +49,36 @@ export default function MyProfile(){
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="1">
-                            <Card.Body>Need to connect to database</Card.Body>
+                            <table class="table table-sm">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Programme</th>
+                                        <th scope="col">Year</th>
+                                       
+                                    </tr>
+
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">JA Company Programme</th>
+                                        <td>2020</td> 
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">JA Personal Spending</th>
+                                        <td>2019</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">JA Finance for Future</th>
+                                        <td>2019</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </Accordion.Collapse>
                     </Card>
                     <Card>
                         <Card.Header>
                             <Accordion.Toggle as={Button} variant="light" eventKey="2">
-                                Your Group
+                                Your Partners
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="2">
