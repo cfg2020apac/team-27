d6788e16c685e0a33dc74d41593d5cdec3b54dfc
ibatra <ibatra@connect.ust.hk>
2020-10-09 19:10:29 +0800

    update connect with utils

--- firebase/connect.py
+++ firebase/connect.py
@@ -6,17 +6,23 @@ import os
 from firebase_admin import firestore, initialize_app, credentials
 
 
-dirname = os.path.dirname(__file__)
+# dirname = os.path.dirname(__file__)
 
-filename = os.path.join(dirname, 'env-firebase-private.json')
-cred = credentials.Certificate(filename)
+# filename = os.path.join(dirname, 'env-firebase-private.json')
+# cred = credentials.Certificate(filename)
 
-credential_path = os.path.join(dirname, 'env.json')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+# credential_path = os.path.join(dirname, 'env.json')
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
-__all__ = ['send_to_firebase', 'update_firebase_snapshot']
+# __all__ = ['send_to_firebase', 'update_firebase_snapshot']
 
-initialize_app(cred)
+# initialize_app(cred)
+
+def connect(cred_file: str):
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    cred_path = os.path.join(ROOT_DIR, cred_file)
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
 
 def add_to_firebase(data, collection : str):
     """
@@ -51,21 +57,61 @@ def update_to_firebase(data, snapshot_id, collection : str):
     spend_time = timedelta(seconds=end - start)
     return spend_time
 
-def get_from_collection(collection: str, document: str):
+def get_collection(collection: str):
+    """
+    collection = class name
+    returns all items from class (list of dicts)
+    """
+
     db = firestore.client()
     doc_ref = db.collection(collection).stream()
-    # {doc.id} => {doc.to_dict()}
-    return doc_ref
+    stream = []
+    for doc in doc_ref:
+        stream.append(doc.to_dict())
+    return stream
 
+def delete_doc(collection: str, document: str):
+    """
+    Delete specific document from collection
+    """
+    db = firestore.client()
+    db.collection(collection).document(document).delete()
+    
+def delete_field(collection: str, document: str, field: str):
+    """
+    Delete specific fields from document in collection
+    """
+    doc_ref = db.collection(collection).document(document)
+    doc_ref.update({
+        field: firestore.DELETE_FIELD
+    })
+
+
+def delete_collection(collection: str, batch_size = 64):
+    """
+    Delete collection of data
+    batch_size set by 64 to default to deal with OOM errors
+    """
+    docs = collection.limit(batch_size).stream()
+    deleted = 0
+
+    for doc in docs:
+        print(f'Deleting doc {doc.id} => {doc.to_dict()}')
+        doc.reference.delete()
+        deleted = deleted + 1
+
+    if deleted >= batch_size:
+        return delete_collection(collection, batch_size)
 
 if __name__ == "__main__":
-    data = {
-        u'name': u'Los Angeles',
-        u'age': 18,
-        u'preference': ["greater_virginia", "hong_kong"]
-    }
-    db = firestore.client()
-    # Add a new doc in collection 'cities' with ID 'LA'
-    # db.collection(u'students').document(u'LA').set(data)
-    for doc in get_from_collection(u'students', ""):
-        print(doc.to_dict())
+
+    #     data = {
+#         u'name': u'Los Angeles',
+#         u'age': 18,
+#         u'preference': ["greater_virginia", "hong_kong"]
+#     }
+#     db = firestore.client()
+#     # Add a new doc in collection 'cities' with ID 'LA'
+#     # db.collection(u'students').document(u'LA').set(data)
+#     for doc in get_from_collection(u'students', ""):
+#         print(doc.to_dict())
