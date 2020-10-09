d75cc5cd999c8b0be249db22557c9c68705e20d6
John Law <johnlaw.po@gmail.com>
2020-10-10 02:27:04 +0800

    finish milestines

--- client/src/components/Milestones.jsx
+++ client/src/components/Milestones.jsx
@@ -1,85 +1,84 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
 import {
     VerticalTimeline,
     VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
-
 const toDoStyle = { background: "rgb(255,140,0)", color: "#fff" };
 const finishedStyle = { background: "rgb(16, 204, 82)", color: "#fff" };
 
-const contents = [
-    {
-        type: "todo",
-        date: "October 2020 Due",
-        content: (
-            <>
-                <h3 className="vertical-timeline-element-title">Prototype A</h3>
-                <h4 className="vertical-timeline-element-subtitle">To-do</h4>
-                <p></p>
-            </>
-        ),
-    },
-    {
-        type: "finished",
-        date: "May 2020",
-        content: (
-            <>
-                <h3 className="vertical-timeline-element-title">
-                    Design thinking workshop II
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">Done</h4>
-                <p>Completed design thinking workshop.</p>
-            </>
-        ),
-    },
-    {
-        type: "finished",
-        date: "April 2020",
-        content: (
-            <>
-                <h3 className="vertical-timeline-element-title">
-                    Design thinking workshop I
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">Done</h4>
-                <p>Completed design thinking workshop.</p>
-            </>
-        ),
-    },
-    {
-        type: "finished",
-        date: "March 2020",
-        content: (
-            <>
-                <h3 className="vertical-timeline-element-title">
-                    Proposal submission
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">Done</h4>
-                <p>Proposal received by company A.</p>
-            </>
-        ),
-    },
-];
-
 export default function Milestones() {
+    const [contents, setContents] = useState([]);
+
+    useEffect(() => {
+        const getMilestones = async () => {
+            const res = await axios.get("http://localhost:8000/milestones/1");
+            setContents(
+                res.data.milestones
+                    .map((t) => {
+                        if (t.type === "finished") {
+                            return {
+                                type: "finished",
+                                date: t.date,
+                                content: {
+                                    title: t.title,
+                                    desc: t.desc,
+                                },
+                            };
+                        } else {
+                            return {
+                                type: "todo",
+                                date: t.date,
+                                content: {
+                                    title: t.title,
+                                    desc: t.desc,
+                                },
+                            };
+                        }
+                    })
+                    .sort((x, y) => {
+                        return y.type.localeCompare(x.type);
+                    })
+            );
+        };
+
+        if (!contents.length) {
+            getMilestones();
+        }
+    }, []);
+
+    if (!contents.length) {
+        return (
+            <div>
+                <Spinner
+                    animation="grow"
+                    variant="success"
+                    style={{ marginTop: "2em", marginBottom: "2em" }}
+                />
+                <br />
+                <Spinner
+                    animation="grow"
+                    variant="success"
+                    style={{ marginBottom: "2em" }}
+                />
+                <br />
+                <Spinner
+                    animation="grow"
+                    variant="success"
+                    style={{ marginBottom: "2em" }}
+                />
+            </div>
+        );
+    }
+
     return (
         <VerticalTimeline>
             {contents.map((t) => {
                 if (t.type === "todo") {
-                    return (
-                        <VerticalTimelineElement
-                            iconStyle={toDoStyle}
-                            contentStyle={toDoStyle}
-                            contentArrowStyle={toDoStyle}
-                            date={t.date}
-                            icon={<FaExclamationCircle />}
-                        >
-                            {t.content}
-                        </VerticalTimelineElement>
-                    );
-                } else {
                     return (
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
@@ -89,7 +88,31 @@ export default function Milestones() {
                             date={t.date}
                             icon={<FaCheckCircle />}
                         >
-                            {t.content}
+                            <h3 className="vertical-timeline-element-title">
+                                {t.content.title}
+                            </h3>
+                            <h4 className="vertical-timeline-element-subtitle">
+                                Done
+                            </h4>
+                            <p>{t.content.desc}</p>
+                        </VerticalTimelineElement>
+                    );
+                } else {
+                    return (
+                        <VerticalTimelineElement
+                            iconStyle={toDoStyle}
+                            contentStyle={toDoStyle}
+                            contentArrowStyle={toDoStyle}
+                            date={t.date}
+                            icon={<FaExclamationCircle />}
+                        >
+                            <h3 className="vertical-timeline-element-title">
+                                {t.content.title}
+                            </h3>
+                            <h4 className="vertical-timeline-element-subtitle">
+                                To-do
+                            </h4>
+                            <p>{t.content.desc}</p>
                         </VerticalTimelineElement>
                     );
                 }
