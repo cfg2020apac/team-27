29c072d96fa0c36094ec6b9d6e626f38888e9b3e
Binay Gurung <binaygurung9@gmail.com>
2020-10-09 16:40:23 +0800

    React-Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import {Route,Switch, Redirect} from 'react-router-dom';
 import './App.css';
-
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Root from './components/Root';
 function App() {
+  const [isLogin,setisLogin] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' render={()=>{
+            return(
+              <Redirect to="/root"/>
+                // isLogin?
+                // <Redirect to="/login"/>:
+                // <Redirect to="/signup"/>
+            )
+        }}/>
+        <Route path="/login"><Login/></Route>
+        <Route path="/signup"><Signup/></Route>
+        <Route path="/root" ><Root/></Route> 
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={App}/>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
