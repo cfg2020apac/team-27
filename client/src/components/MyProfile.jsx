0bab62ff0610557bf52cc3d584098e2356c2c15b
Jeff <jeffli351999@gmail.com>
2020-10-09 23:37:11 +0800

    MyProfile

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import { Accordion, Card, Container, Button, Row, Col, Image } from 'react-bootstrap';
 
 export default function MyProfile(){
     return(
         <div>
             <Container fluid>
-                This is Profile
+                <Container>
+                    <Row>
+                        <Col xs={100} md={60}>
+                            <Image src="" rounded />
+                        </Col>
+                   
+                    </Row>
+                </Container>
+
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                                About you
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>Need to connect to database</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="1">
+                                Past Experience
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>Need to connect to database</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="2">
+                                Your Group
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>Need to connect to database</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
             </Container>
         </div>
     )
